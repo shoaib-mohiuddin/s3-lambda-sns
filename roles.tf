a717eedbd51ebc62ff5216921e04b0d6cb07332e
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-09-13 22:07:23 +0530

    Attaches requisite roles to lambda

--- roles.tf
+++ roles.tf
@@ -21,3 +21,119 @@ resource "aws_iam_role" "lambda_assume_role" {
     tag-key = "lambda-assume-role"
   }
 }
+
+#----------------------------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "s3_access_for_lambda" {
+  statement {
+    sid = "LambdaAccessS3Bucket"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.word_count_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.word_count_bucket.bucket}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "s3_access_lambda" {
+  name = "s3_access_for_lambda"
+  description = "Allow Lambda function to access S3 bucket and objects"
+  policy = data.aws_iam_policy_document.s3_access_for_lambda.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_access_s3" {
+  role = aws_iam_role.lambda_assume_role.name
+  policy_arn = aws_iam_policy.s3_access_lambda.arn
+}
+
+#---------------------------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "sns_access_for_lambda" {
+  statement {
+    sid = "LambdaAccessSNS"
+    actions = ["sns:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "sns_access_lambda" {
+  name = "sns_access_for_lambda"
+  description = "Allow Lambda function to send message to SNS topic"
+  policy = data.aws_iam_policy_document.sns_access_for_lambda.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_access_sns" {
+  role = aws_iam_role.lambda_assume_role.name
+  policy_arn = aws_iam_policy.sns_access_lambda.arn
+}
+
+#---------------------------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "cw_access_for_lambda" {
+  statement {
+    sid = "LambdaAccessCW"
+    actions = [
+        "autoscaling:Describe*",
+        "cloudwatch:*",
+        "logs:*",
+        "sns:*",
+        "iam:GetPolicy",
+        "iam:GetPolicyVersion",
+        "iam:GetRole"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid = "LambdaAccessCWEvents"
+    actions = [
+        "iam:CreateServiceLinkedRole"
+    ]
+    resources = [
+        "arn:aws:iam::*:role/aws-service-role/events.amazonaws.com/AWSServiceRoleForCloudWatchEvents*"
+    ]
+    condition {
+      test     = "StringLike"
+      variable = "iam:AWSServiceName"
+      values   = ["events.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "cw_access_lambda" {
+  name = "cw_access_for_lambda"
+  description = "Allow Lambda function to send logs to Cloudwatch"
+  policy = data.aws_iam_policy_document.cw_access_for_lambda.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_access_cw" {
+  role = aws_iam_role.lambda_assume_role.name
+  policy_arn = aws_iam_policy.cw_access_lambda.arn
+}
+
+#----------------------------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "lambda_basic_execution" {
+  statement {
+    sid = "LambdaBasicExecutionRole"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_basic_exec" {
+  name = "lambda_basic_execution_role"
+  description = "Basic execution role for lambda"
+  policy = data.aws_iam_policy_document.lambda_basic_execution.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec_role" {
+  role = aws_iam_role.lambda_assume_role.name
+  policy_arn = aws_iam_policy.lambda_basic_exec.arn
+}
